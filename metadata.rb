ca242f412a381e0ccdaf270afad2f532aedaa318
John McDowall <john@mcdowall.info>
2014-10-21 12:27:21 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "devops-memcached-cookbook"
+maintainer        "John McDowall"
+maintainer_email  "john@kantan.io"
+description       "Simple memcached installation"
+version           "0.0.1"
+
+recipe "devops-memcached-cookbook", "Installs memcached"
+
+supports "ubuntu"
